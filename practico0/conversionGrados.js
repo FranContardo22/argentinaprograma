06c932a3bc1fba1c534ea689b5430484b6694ba9
FranContardo22 <fran22cab@gmail.com>
2023-05-28 23:33:45 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * (5 / 9);
+console.log(celsius);
+// 50 F equivalen a 10 C //
+
+fahrenheit = 75;
+celsius = (fahrenheit - 32) * (5 / 9);
+console.log(celsius);
+// 75 F equivalen a 23.88 C//
